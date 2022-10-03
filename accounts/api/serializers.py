39a4754d7855aed266719df202dd332858d37815
KimGJHC <gongjinghao.cheng.github@gmail.com>
2022-10-02 21:34:40 -0400

    add logout

--- accounts/api/serializers.py
+++ accounts/api/serializers.py
@@ -1,8 +1,16 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
-# encode user object to JSON format
+# 1. encode user object to JSON format for frontend
+# 2. validate user data
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('username', 'email')
+        fields = ('id', 'username', 'email')
+
+
+
+class LoginSerializer(serializers.Serializer):
+    # check if username or password is empty
+    username = serializers.CharField()
+    password = serializers.CharField()
--- accounts/api/views.py
+++ accounts/api/views.py
@@ -1,8 +1,14 @@
 from django.contrib.auth.models import User
+from django.contrib.auth import (
+    login as django_login,
+    logout as django_logout,
+    authenticate as django_authenticate
+)
+
 from rest_framework import viewsets, permissions
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from accounts.api.serializers import UserSerializer
+from accounts.api.serializers import UserSerializer, LoginSerializer
 
 # modelViewSet, to provide user api to read user data
 class UserViewSet(viewsets.ModelViewSet): # (ReadOnlyModelViewSet)
@@ -14,9 +20,25 @@ class UserViewSet(viewsets.ModelViewSet): # (ReadOnlyModelViewSet)
 class AccountViewSet(viewsets.ViewSet):
     # need to do CRUD ourselves
 
-    @action(methods=['GET'], detail=False)
+    @action(methods=['GET'], detail=False) # detail = True if this action is on object
     def login_status(self, request):
         data = {'has_logged_in': request.user.is_authenticated}
         if request.user.is_authenticated:
             data['user'] = UserSerializer(request.user).data
         return Response(data)
+
+    @action(methods=['POST'], detail=False)
+    def logout(self, request):
+        django_logout(request)
+        return Response({'success': True})
+
+    @action(methods=['POST'], detail=False)
+    def login(self, request):
+        # get username and password from request
+        serializer = LoginSerializer(data=request.data)
+        if not serializer.is_valid():
+            return Response({
+                "success": False,
+                "message": "Please check input",
+                "errors": serializer.errors,
+            }, status=400)
