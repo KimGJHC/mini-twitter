7d9297b2d5525a93f83127612cfac210a956b252
KimGJHC <gongjinghao.cheng.github@gmail.com>
2022-10-23 16:28:58 -0400

    add tweet pagination

--- tweets/api/views.py
+++ tweets/api/views.py
@@ -9,6 +9,7 @@ from tweets.api.serializers import (
 )
 from tweets.models import Tweet
 from utils.decorators import required_params
+from utils.paginations import EndlessPagination
 
 class TweetViewSet(viewsets.GenericViewSet,
                    viewsets.mixins.CreateModelMixin,
@@ -17,6 +18,7 @@ class TweetViewSet(viewsets.GenericViewSet,
     # API end point to let user create and list tweets
     queryset = Tweet.objects.all()
     serializer_class = TweetSerializerForCreate
+    pagination_class = EndlessPagination
 
     # check permission to actions
     def get_permissions(self):
@@ -37,13 +39,14 @@ class TweetViewSet(viewsets.GenericViewSet,
         tweets = Tweet.objects.filter(
             user_id=request.query_params['user_id']
         ).order_by('-created_at')
+        tweets = self.paginate_queryset(tweets)
         serializer = TweetSerializer(
             tweets,
             context={'request': request},
             many=True,
         ) # return a list of dict
 
-        return Response({'tweets': serializer.data})
+        return self.get_paginated_response(serializer.data)
 
     def create(self, request):
         serializer = TweetSerializerForCreate(
