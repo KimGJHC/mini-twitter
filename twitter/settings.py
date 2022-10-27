2428a4bc734fb5a4166a0d544fe14e2757d3f0ac
KimGJHC <gongjinghao.cheng.github@gmail.com>
2022-10-26 22:25:57 -0400

    add-redis

--- twitter/settings.py
+++ twitter/settings.py
@@ -178,6 +178,11 @@ CACHES = {
     },
 }
 
+REDIS_HOST = '127.0.0.1'
+REDIS_PORT = 6379
+REDIS_DB = 0 if TESTING else 1
+REDIS_KEY_EXPIRE_TIME = 7 * 86400
+
 try:
     from .local_settings import *
 except:
