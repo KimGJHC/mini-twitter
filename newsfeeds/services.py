5be4c5702ecbd5ce97c51649e17fb56322930e37
KimGJHC <gongjinghao.cheng.github@gmail.com>
2022-10-28 21:01:23 -0400

    add newsfeed cache

--- newsfeeds/services.py
+++ newsfeeds/services.py
@@ -1,5 +1,7 @@
 from friendships.services import FriendshipService
 from newsfeeds.models import NewsFeed
+from twitter.cache import USER_NEWSFEEDS_PATTERN
+from utils.redis_helper import RedisHelper
 
 class NewsFeedService(object):
     # service usually has class method
@@ -14,3 +16,19 @@ class NewsFeedService(object):
         ]
         newsfeeds.append(NewsFeed(user=tweet.user, tweet=tweet))
         NewsFeed.objects.bulk_create(newsfeeds)
+
+        # bulk_create does not signify post_save
+        for newsfeed in newsfeeds:
+            cls.push_newsfeed_to_cache(newsfeed)
+
+    @classmethod
+    def get_cached_newsfeeds(cls, user_id):
+        queryset = NewsFeed.objects.filter(user_id=user_id).order_by('-created_at')
+        key = USER_NEWSFEEDS_PATTERN.format(user_id=user_id)
+        return RedisHelper.load_objects(key, queryset)
+
+    @classmethod
+    def push_newsfeed_to_cache(cls, newsfeed):
+        queryset = NewsFeed.objects.filter(user_id=newsfeed.user_id).order_by('-created_at')
+        key = USER_NEWSFEEDS_PATTERN.format(user_id=newsfeed.user_id)
+        RedisHelper.push_object(key, newsfeed, queryset)
